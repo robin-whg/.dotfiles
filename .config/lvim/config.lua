d7858d61e1838e5163cf2fcc5443e4d577f17e0e
robin-whg <mail@robin-whg.com>
2022-05-12 21:18:16 +0200

    add minimap

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -236,6 +236,16 @@ lvim.builtin.treesitter = {
       "#7dcfff",
       "#a9b1d6",
     }
+  {
+    'wfxr/minimap.vim',
+    run = "cargo install --locked code-minimap",
+    -- cmd = {"Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight"},
+    -- vim.cmd("let g:minimap_auto_start_win_enter = 1")
+    config = function()
+      vim.cmd("let g:minimap_width = 10")
+      vim.cmd("let g:minimap_auto_start = 1")
+      vim.cmd("let g:minimap_git_colors = 1")
+    end,
   },
 }
 
