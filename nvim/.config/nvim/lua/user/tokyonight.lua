f4cfb21d4aed21a928e78bf7aa2e254e133669e0
robin-whg <mail@robin-whg.com>
2022-06-16 19:37:19 +0200

    style

--- nvim/.config/nvim/lua/user/tokyonight.lua
+++ nvim/.config/nvim/lua/user/tokyonight.lua
@@ -2,6 +2,7 @@ local colorscheme = "tokyonight"
 
 local colors = require("tokyonight.colors").setup({})
 vim.g.tokyonight_colors = { bg_float = colors.bg }
+vim.g.tokyonight_dark_sidebar = false
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
