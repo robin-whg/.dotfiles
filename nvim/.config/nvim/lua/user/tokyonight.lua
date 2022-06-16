50365055086a051d0fc96f8b07e69128755bdab1
robin-whg <mail@robin-whg.com>
2022-06-16 13:56:16 +0200

    change color for floating windows

--- nvim/.config/nvim/lua/user/tokyonight.lua
+++ nvim/.config/nvim/lua/user/tokyonight.lua
@@ -1,6 +1,7 @@
 local colorscheme = "tokyonight"
 
-vim.g.tokyonight_style = "storm"
+local colors = require("tokyonight.colors").setup({})
+vim.g.tokyonight_colors = { bg_float = colors.bg }
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
