53fd1726948890068fc9a47ee22965db76e479ea
robin-whg <mail@robin-whg.com>
2022-06-18 23:53:50 +0200

    change color with config option

--- nvim/.config/nvim/lua/user/tokyonight.lua
+++ nvim/.config/nvim/lua/user/tokyonight.lua
@@ -1,12 +1,10 @@
 local colorscheme = "tokyonight"
 
-local colors = require("tokyonight.colors").setup({})
-vim.g.tokyonight_colors = { bg_float = colors.bg }
 vim.g.tokyonight_dark_sidebar = false
+vim.g.tokyonight_dark_float = false
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
+	vim.notify("colorscheme " .. colorscheme .. " not found!")
+	return
 end
-
