1ddbe55d5ef664901fdb2c65060cf05feee78d75
robin-whg <mail@robin-whg.com>
2022-06-18 21:02:37 +0200

    avoid -

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,13 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.tokyonight"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.nvim-tree"
-require "user.lualine"
-require "user.nvim-colorizer"
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.tokyonight")
+require("user.cmp")
+require("user.lsp")
+require("user.telescope")
+require("user.treesitter")
+require("user.autopairs")
+require("user.comment")
+require("user.nvimtree")
+require("user.lualine")
+require("user.nvimcolorizer")
