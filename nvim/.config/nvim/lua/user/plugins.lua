27208f96a87c3bd37acaf3f6da007c5b45e166d6
robin-whg <mail@robin-whg.com>
2022-06-19 19:09:28 +0200

    add lsp-signature

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -81,6 +81,7 @@ return packer.startup(function(use)
 	-- LSP
 	use("neovim/nvim-lspconfig") -- enable LSP
 	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
+	use("ray-x/lsp_signature.nvim")
 
 	use("lukas-reineke/indent-blankline.nvim")
 
