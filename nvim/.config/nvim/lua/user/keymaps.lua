9c6a346f9f42d57f780197aacb25a19be0d894dc
robin-whg <mail@robin-whg.com>
2022-06-22 20:36:40 +0200

    better keymaps and doc

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -25,25 +25,22 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- file expl
-keymap("n", "<leader>e", ":Lex 20<CR>", opts)
-
 -- Resize with arrows
-keymap("n", "<C-Up>", ":resize +2<CR>", opts)
-keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Buffer Navigation
 keymap("n", "Tab", ":bnext<CR>", opts)
 keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
-keymap("n", "<C-b>", "<C-^>", opts)
+keymap("n", "<C-b>", "<C-^>", opts) -- overwrites go up one page
 
 -- Standard save
 keymap("n", "<C-s>", ":w<CR>", opts)
 
 -- Close buffer
-keymap("n", "<C-x>", ":bd<CR>", opts)
+keymap("n", "<C-x>", ":bd<CR>", opts) -- overwrites decrement. Increment <C-a> is tmux prefix.
 
 -- Quit
 keymap("n", "<C-q>", ":q<CR>", opts)
@@ -54,6 +51,13 @@ keymap("n", "x", '"_x', opts)
 
 -- Y to behave like D and C
 keymap("n", "Y", "yg$", opts)
+
+-- Paste last thing yanked, not deleted
+keymap("n", "<c-p>", '"0p', opts)
+
+-- Easier $ and ^
+keymap("n", "L", "$", opts) -- overwrites go to last visible line
+keymap("n", "H", "^", opts) -- overwrites go to first visible line
 
 -- Insert --
 -- Press jk fast to exit
@@ -84,7 +88,7 @@ keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 -- Extension keymaps
-keymap("n", "<C-f>", "<cmd>lua require'telescope.builtin'.git_files()<cr>", opts)
+keymap("n", "<C-f>", "<cmd>lua require'telescope.builtin'.git_files()<cr>", opts) -- overwrites go down one page
 
 keymap("x", "<C-_>", "<Plug>(comment_toggle_linewise_visual)", opts)
 keymap("n", "<C-_>", "<Plug>(comment_toggle_current_linewise)", opts)
