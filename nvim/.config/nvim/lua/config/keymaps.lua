af2a2ea9a48bb435af7828c8fbc1f4eb6d49b0de
robin-whg <mail@robin-whg.com>
2023-01-27 12:00:37 +0100

    tweaks

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,21 +1,10 @@
-local opts = { noremap = true, silent = true }
-
-local term_opts = { silent = true }
-
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 
--- Modes
---   normal_mode = "n",
---   insert_mode = "i",
---   visual_mode = "v",
---   visual_block_mode = "x",
---   term_mode = "t",
---   command_mode = "c",
+-- Shorten function name
+local keymap = vim.keymap.set
+
+local opts = { noremap = true, silent = true }
 
 -- Normal Mode --
 -- Better window navigation
@@ -23,6 +12,12 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+-- center navigation
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+keymap("n", "J", "mzJ`z", opts)
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
@@ -36,24 +31,23 @@ keymap("n", ";", ":", opts)
 -- Buffer Navigation
 keymap("n", "Tab", ":bnext<CR>", opts)
 keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
--- keymap("n", "<C-b>", "<C-^>", opts) -- overwrites go up one page
--- TODO: rebind jumpmap
 -- Standard save
 keymap("n", "<C-s>", ":w<CR>", opts)
--- Close buffer
-keymap("n", "<C-x>", ":bd<CR>", opts) -- overwrites decrement; increment <C-a> is tmux prefix.
 -- Quit
 keymap("n", "<C-q>", ":q<CR>", opts)
 keymap("n", "<C-Q>", ":q!<CR>", opts)
 -- don't yank chars deleted with x
 keymap("n", "x", '"_x', opts)
--- Y to behave like D and C
-keymap("n", "Y", "yg$", opts)
 -- Paste last thing yanked, not deleted
 keymap("n", "<alt-p>", '"0p', opts)
+-- Y to behave like D and C
+keymap("n", "Y", "yg$", opts)
 -- Easier $ and ^
 keymap("n", "L", "$", opts) -- overwrites go to last visible line
 keymap("n", "H", "^", opts) -- overwrites go to first visible line
+
+keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace current word" })
+keymap("n", "<leader>x", "<cmd>!chmod +x %<CR>", { desc = "Make file executable", noremap = true })
 
 -- Insert --
 -- Press jk fast to exit
@@ -66,8 +60,10 @@ keymap("v", ">", ">gv", opts)
 -- Move text up and down
 keymap("v", "J", ":m .+1<CR>==", opts)
 keymap("v", "K", ":m .-2<CR>==", opts)
--- Paste without yanking selection
+-- Paste without yanking
 keymap("v", "p", '"_dP', opts)
+-- Delete without yanking
+keymap("v", "<leader>d", "\"_d")
 
 -- Visual Block --
 -- Move text up and down
