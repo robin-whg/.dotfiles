d627e7fc6755719f2958c007e88c509515af7b8f
robin-whg <mail@robin-whg.com>
2023-01-27 12:00:54 +0100

    add lualine

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    { "kyazdani42/nvim-web-devicons" }
+  },
+  config = function()
+    local branch = {
+      "branch",
+      icons_enabled = true,
+      icon = "",
+    }
+    require("lualine").setup({
+      options = {
+        theme = "tokyonight",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {},
+        always_divide_middle = true,
+        globalstatus = true,
+      },
+      sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'mode', branch, 'diff', 'diagnostics', 'filename' },
+
+        lualine_x = { 'filetype', 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {}
+      },
+    })
+  end
+}
