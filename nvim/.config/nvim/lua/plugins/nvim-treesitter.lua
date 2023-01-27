3be4d8663530ae35ca34ee383a40ad4e20f38258
robin-whg <mail@robin-whg.com>
2023-01-27 16:16:49 +0100

    lazy load treesitter correctly

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,12 +1,33 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ':TSUpdate',
+  event = "BufReadPost",
   config = function()
     require('nvim-treesitter.configs').setup({
-      ensure_installed = { "c", "lua", "vim", "python", "javascript", "typescript", "go", "rust" },
-      highlight = {
-        enabled = true
-      }
+      highlight = { enable = true },
+      indent = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+      ensure_installed = {
+        "bash",
+        "help",
+        "html",
+        "css",
+        "javascript",
+        "json",
+        "json5",
+        "jsonc",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "typescript",
+        "go",
+        "rust",
+        "vim",
+        "yaml",
+      },
     })
   end
 }
