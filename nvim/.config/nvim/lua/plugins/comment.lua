611e1413b41747e11a016fc7e1288fc8bea063e9
robin-whg <mail@robin-whg.com>
2023-01-29 20:04:44 +0100

    change keymaps

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -5,25 +5,11 @@ return {
     require("Comment").setup({
       toggler = {
         ---Line-comment toggle keymap
-        line = '<leader>cc',
-        ---Block-comment toggle keymap
-        block = '<leader>bc',
+        line = '<C-_>',
       },
-      ---LHS of operator-pending mappings in NORMAL and VISUAL mode
       opleader = {
         ---Line-comment keymap
-        line = '<leader>c',
-        ---Block-comment keymap
-        block = '<leader>b',
-      },
-      ---LHS of extra mappings
-      extra = {
-        ---Add comment on the line above
-        above = '<leader>cO',
-        ---Add comment on the line below
-        below = '<leader>co',
-        ---Add comment at the end of line
-        eol = '<leader>cA',
+        line = '<C-_>',
       },
     })
   end
