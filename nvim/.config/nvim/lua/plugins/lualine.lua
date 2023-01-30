5ddab0a0aa4c7bdcb4e516f8507af382ed937d4a
robin-whg <mail@robin-whg.com>
2023-01-30 21:03:52 +0100

    list lsp servers in lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -10,6 +10,43 @@ return {
       icons_enabled = true,
       icon = "",
     }
+    local lsp = {
+      function(msg)
+        msg = msg or "LS Inactive"
+        local buf_clients = vim.lsp.buf_get_clients()
+        if next(buf_clients) == nil then
+          if type(msg) == "boolean" or #msg == 0 then
+            return "LS Inactive"
+          end
+          return msg
+        end
+        -- local buf_ft = vim.bo.filetype
+        local buf_client_names = {}
+
+        -- add client
+        for _, client in pairs(buf_clients) do
+          if client.name ~= "null-ls" then
+            table.insert(buf_client_names, client.name)
+          end
+        end
+
+        -- add formatter
+        -- local formatters = require "lvim.lsp.null-ls.formatters"
+        -- local supported_formatters = formatters.list_registered(buf_ft)
+        -- vim.list_extend(buf_client_names, supported_formatters)
+
+        -- add linter
+        -- local linters = require "lvim.lsp.null-ls.linters"
+        -- local supported_linters = linters.list_registered(buf_ft)
+        -- vim.list_extend(buf_client_names, supported_linters)
+
+        local unique_client_names = vim.fn.uniq(buf_client_names)
+
+        local language_servers = "[" .. table.concat(unique_client_names, ", ") .. "]"
+
+        return language_servers
+      end,
+    }
 
     require("lualine").setup({
       options = {
@@ -25,7 +62,7 @@ return {
         lualine_b = {},
         lualine_c = { 'mode', branch, 'filename', 'diff', 'diagnostics' },
 
-        lualine_x = { 'filetype', 'location' },
+        lualine_x = { lsp, 'filetype', 'location' },
         lualine_y = {},
         lualine_z = {}
       },
