3cfa0de1ab0d3879eff2a925260493c41077d1dc
robin-whg <mail@robin-whg.com>
2023-12-19 21:20:47 +0100

    Update Wezterm configuration

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,22 +7,22 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Catppuccin Macchiato'
-config.font = wezterm.font 'FiraCode Nerd Font'
-config.font_size = 12.0
+config.color_scheme = "Tokyo Night Moon"
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 12
 config.font_shaper = "Harfbuzz"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 -- and finally, return the configuration to wezterm
