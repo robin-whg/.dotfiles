3eff43fcfce61a4ad89135596855c08a0da8d1f1
robin-whg <mail@robin-whg.com>
2023-12-22 15:27:43 +0100

    Add CSS and SCSS to ensure_installed list in treesitter.lua

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "vue", "prisma" })
+      vim.list_extend(opts.ensure_installed, { "vue", "prisma", "css", "scss" })
     end
   end,
 }
