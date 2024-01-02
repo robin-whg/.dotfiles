b26eb5c4f38e9eabe0c7e8af228affa6e2731a25
robin-whg <mail@robin-whg.com>
2024-01-02 20:08:03 +0100

    back to real font

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,7 +14,7 @@ end
 
 -- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night Moon"
-config.font = wezterm.font("ComicCode Nerd Font")
+config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12
 config.font_shaper = "Harfbuzz"
 config.hide_tab_bar_if_only_one_tab = true
