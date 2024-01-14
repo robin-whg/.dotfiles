cafcc3a8b3e36cc36692fa7cb77441149fdb3b57
robin-whg <mail@robin-whg.com>
2024-01-14 21:29:07 +0100

    actually set the coloscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,19 +1,27 @@
 return {
-  "folke/tokyonight.nvim",
-  opts = {
-    style = "moon",
-    styles = {
-      sidebars = "normal",
-      floats = "normal",
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "moon",
+      styles = {
+        sidebars = "normal",
+        floats = "normal",
+      },
+      on_highlights = function(hl, c)
+        hl.winbar = {
+          bg = c.bg,
+        }
+        hl.winbarnc = {
+          fg = c.fg_dark,
+          bg = c.bg,
+        }
+      end,
     },
-    on_highlights = function(hl, c)
-      hl.winbar = {
-        bg = c.bg,
-      }
-      hl.winbarnc = {
-        fg = c.fg_dark,
-        bg = c.bg,
-      }
-    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
   },
 }
