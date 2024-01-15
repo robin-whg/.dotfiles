9129dddc692957809cb7be62bc734f7d3b28f6a4
robin-whg <mail@robin-whg.com>
2024-01-15 22:53:30 +0100

    chore

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,18 +1,11 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night Moon"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 12
@@ -27,5 +20,4 @@ config.window_padding = {
 config.initial_rows = 48
 config.initial_cols = 160
 
--- and finally, return the configuration to wezterm
 return config
