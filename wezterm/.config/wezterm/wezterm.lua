c1ffb57f5af313b4d05f10ec84fa6af8c2a70939
robin-whg <mail@robin-whg.com>
2024-01-22 20:46:17 +0100

    move config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,19 +3,20 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
+config.native_macos_fullscreen_mode = true
 config.color_scheme = "Tokyo Night Moon"
 config.font = wezterm.font("FiraCode Nerd Font")
-config.font_size = 11
+config.font_size = 12
 config.font_shaper = "Harfbuzz"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 4,
-  right = 4,
-  top = 4,
-  bottom = 4,
+	left = 4,
+	right = 4,
+	top = 4,
+	bottom = 4,
 }
 config.initial_rows = 48
 config.initial_cols = 160
