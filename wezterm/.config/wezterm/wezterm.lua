30d3b284f9386276502cc0df1176235e6593c188
robin-whg <mail@robin-whg.com>
2024-01-22 16:23:36 +0100

    move wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 config.color_scheme = "Tokyo Night Moon"
@@ -12,10 +12,10 @@ config.font_size = 11
 config.font_shaper = "Harfbuzz"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-  left = 4,
-  right = 4,
-  top = 4,
-  bottom = 4,
+	left = 4,
+	right = 4,
+	top = 4,
+	bottom = 4,
 }
 config.initial_rows = 48
 config.initial_cols = 160
