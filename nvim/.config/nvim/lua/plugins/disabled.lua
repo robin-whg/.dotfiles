a7e55b46320d93140eefb64ad182f36a3a4f6c7d
robin-whg <mail@robin-whg.com>
2024-01-24 15:58:24 +0100

    add nvim-tree

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   -- disable trouble
   { "akinsho/bufferline.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
