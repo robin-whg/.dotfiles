3c9ab5aba0a88b1d1a7861f15f6725e15ad7ba1f
robin-whg <mail@robin-whg.com>
2024-07-04 16:17:01 +0200

    add tmux navigator

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,13 +4,13 @@
 
 local map = vim.keymap.set
 
-map("i", "jk", "<esc>", { silent = true })           -- `jk` to escape
-map({ "n", "v" }, ";", ":", { silent = true })       -- easier to type `:`
-map("n", "Y", "yg$", { silent = true })              -- `Y` to behave like `D` and `C`
-map("n", "<S-l>", "$", { silent = true })            -- move to end of line
-map("n", "<S-h>", "^", { silent = true })            -- move to beginning of line
+map("i", "jk", "<esc>", { silent = true }) -- `jk` to escape
+map({ "n", "v" }, ";", ":", { silent = true }) -- easier to type `:`
+map("n", "Y", "yg$", { silent = true }) -- `Y` to behave like `D` and `C`
+map("n", "<S-l>", "$", { silent = true }) -- move to end of line
+map("n", "<S-h>", "^", { silent = true }) -- move to beginning of line
 
-map("n", "<C-q>", "<cmd>qa<cr>", { silent = true })  -- quicker `<leader>qq`
+map("n", "<C-q>", "<cmd>qa<cr>", { silent = true }) -- quicker `<leader>qq`
 map("n", "<C-b>", "<cmd>e #<cr>", { silent = true }) -- quicker `<leader>bb`
 
 local del = vim.keymap.del
@@ -20,3 +20,9 @@ del("n", "<C-_>")
 del("n", "<c-/>")
 del("n", "<leader>ft")
 del("n", "<leader>fT")
+
+map({ "i", "n", "v" }, "<C-k>", "<cmd>TmuxNavigateUp<cr><esc>", { desc = "Move cursor to top pane" })
+map({ "i", "n", "v" }, "<C-j>", "<cmd>TmuxNavigateDown<cr><esc>", { desc = "Move cursor to bottom pane" })
+map({ "i", "n", "v" }, "<C-h>", "<cmd>TmuxNavigateLeft<cr><esc>", { desc = "Move cursor to left pane" })
+map({ "i", "n", "v" }, "<C-l>", "<cmd>TmuxNavigateRight<cr><esc>", { desc = "Move cursor to right pane" })
+map({ "i", "n", "v" }, "<C-\\>", "<cmd>TmuxNavigatePrevious<cr><esc>", { desc = "Move cursor to previous pane" })
