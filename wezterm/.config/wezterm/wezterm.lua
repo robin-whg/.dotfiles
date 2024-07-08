07a23162284da86ae92739e551d1a8c7b5b42aef
robin-whg <mail@robin-whg.com>
2024-07-08 19:56:14 +0200

    style

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
@@ -20,13 +20,14 @@ config.font_size = 11
 config.font_shaper = "Harfbuzz"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-	left = 2,
-	right = 2,
-	top = 0,
-	bottom = 0,
+  left = 2,
+  right = 2,
+  top = 0,
+  bottom = 0,
 }
 config.initial_rows = 48
 config.initial_cols = 160
 
 -- and finally, return the configuration to wezterm
 return config
+
