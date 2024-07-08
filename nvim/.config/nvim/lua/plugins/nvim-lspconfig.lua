77483523bacfe042404d3063b72161d077a30b0d
robin-whg <mail@robin-whg.com>
2024-07-08 17:00:55 +0200

    spell

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,8 @@
-local util = require("lspconfig.util")
-
+-- local words = {}
+-- for word in io.open(vim.fn.stdpath("config") .. "/spell/en.utf-8.add", "r"):lines() do
+--   table.insert(words, word)
+-- end
+--
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -12,5 +15,20 @@ return {
         prefix = "",
       },
     },
+    -- servers = {
+    --   ltex = {
+    --     settings = {
+    --       ltex = {
+    --         language = "de-DE",
+    --         disabledRules = {
+    --           ["de-DE"] = { "WHITESPACE_RULE" },
+    --         },
+    --         dictionary = {
+    --           ["de-DE"] = words,
+    --         },
+    --       },
+    --     },
+    --   },
+    -- },
   },
 }
