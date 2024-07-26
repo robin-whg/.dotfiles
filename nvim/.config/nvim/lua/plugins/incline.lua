47f87e8f773a311d3b904a74f7d114f6ada5a0ee
robin-whg <mail@robin-whg.com>
2024-07-26 21:02:16 +0200

    make more prominent

--- nvim/.config/nvim/lua/plugins/incline.lua
+++ nvim/.config/nvim/lua/plugins/incline.lua
@@ -1,14 +1,26 @@
 return {
   "b0o/incline.nvim",
   config = function()
+    local devicons = require 'nvim-web-devicons'
     require("incline").setup({
       window = {
         margin = {
           horizontal = 0,
           vertical = 0,
         },
-        padding = 0,
+        padding = 0
       },
+      render = function(props)
+        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
+
+        return {
+          ' ',
+          filename,
+          ' ',
+          guifg = '#82aaff',
+          guibg = '#3b4261',
+        }
+      end,
     })
   end,
   -- Optional: Lazy load Incline
